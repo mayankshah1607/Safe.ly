66f2a0336476e8f4082b49490c73b993f985c476
mayankshah1607 <mayankshah1614@gmail.com>
2018-03-30 22:55:05 +0530

    Updated sign in and sign up

--- app/src/main/java/com/example/safely/SignIn.java
+++ app/src/main/java/com/example/safely/SignIn.java
@@ -1,13 +1,28 @@
 package com.example.safely;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignIn extends AppCompatActivity {
+    Button SignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        SignUp = (Button) findViewById(R.id.buttonsignup);
+        SignUp.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent newIntent = new Intent(SignIn.this,SignUp.class);
+                        startActivity(newIntent);
+                    }
+                }
+        );
     }
 }
